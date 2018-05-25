5b27c51d98b0dd64c14bcdfa019ef6d508a758d4
Kirill Kobylyanskiy <kirill.kobylyanskiy@emlid.com>
2018-05-25 21:56:19 +0300

    pb: fix input parameter in GetOperations

--- main.go
+++ main.go
@@ -2,64 +2,14 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log"
 	"net"
 
-	"github.com/dgraph-io/dgo"
-	"github.com/dgraph-io/dgo/protos/api"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
 	pb "github.com/kobylyanskiy/dgraph-api/dgraph"
 )
-
-func newClient() *dgo.Dgraph {
-	d, err := grpc.Dial("dgraph-server-public:9080", grpc.WithInsecure())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return dgo.NewDgraphClient(
-		api.NewDgraphClient(d),
-	)
-}
-
-func setup(c *dgo.Dgraph) {
-	err := c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			codename: string @index(term) .
-			rating: int .
-		`,
-	})
-}
-
-func runTxn(c *dgo.Dgraph) {
-	txn := c.NewTxn()
-	defer txn.Discard()
-	const q = `
-		{
-			all(func: anyofterms(name, "NAME")) {
-				uid
-				rating
-			}
-		}
-	`
-	resp, err := txn.Query(context.Background(), q)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var decode struct {
-		All []struct {
-			Uid    string
-			Rating int
-		}
-	}
-	if err := json.Unmarshal(resp.GetJson(), &decode); err != nil {
-		log.Fatal(err)
-	}
-}
 
 const (
 	port = ":50051"
